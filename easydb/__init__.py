e6b360204129842e2efd7dd690fd627b1a11e38a
Ben Dowling <ben.m.dowling@gmail.com>
2013-03-04 20:03:18 -0800

    Update __init__.py

--- easydb/__init__.py
+++ easydb/__init__.py
@@ -3,11 +3,12 @@ import sqlite3
 
 class EasyDB:
     def __init__(self, filename, schema = None, **kwargs):
-        if os.path.exists(filename) or schema:
-            self.connection = sqlite3.connect(filename)
-        elif not schema:
+        exists = os.path.exists(filename)
+        if !exists and !schema:
             raise Exception, "The specified database file does not exist, and you haven't provided a schema"
-        else:
+    
+        self.connection = sqlite3.connect(filename)
+        if !exists:
             for table_name, fields in schema.items():
                 query = "CREATE TABLE %s (%s)" % (table_name, ", ".join(fields))
                 self.query(query)
